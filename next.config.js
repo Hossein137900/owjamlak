f1b22be8badd617e3d0ee3e1e8ca7f74a9ec4038
wolfix1234 <130220073+wolfix1234@users.noreply.github.com>
2025-10-09 12:38:59 +0330

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,19 @@ const nextConfig = {
   output: 'standalone', // 👈 add this
 
   images: {
-    domains: ['localhost', 'oujamlak'],
+    domains: ['localhost', 'oujamlak.ir'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'oujamlak.ir',
+        pathname: '/api/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        pathname: '/api/**',
+      },
+    ],
   },
 
   async rewrites() {
